0a637ac3de57d243b1d601b87d1ea2af53c862eb
youme <clis9498@naver.com>
2017-01-09 23:34:09 +0900

    2017-01-09 c언어 프로그래밍 printf 예재

--- printf_ex.c
+++ printf_ex.c
@@ -2,8 +2,45 @@
 
 int main()
 {
+
 	int a = 10;
 	int b = 20;
 
 	printf("%d + %d = %d\n", a, b, a + b);
+
+	printf("power\n");
+	printf("김재원\n");
+
+
+	//ctrl + f7 :compile  exam1.c->exam.o(obj)
+	//	f7 : linking  exam.exe
+	//	ctrl + f5 : execute  exam.exe의 실행(더블클릭)
+
+	printf("%d\n", 1234);
+
+	printf("내나이는: %d\n", 20);
+
+
+
+	printf("10\n");    //문자열 10을 출력
+	printf("%d\n", 10); //정수 10을 출력
+	printf("%d\n", a);  //정수 10을 담은 a를 출력
+	printf("%d\n", 3 + 7);
+	printf("%d          %d\n", a, 10);
+
+
+
+	printf("이름: 김유미\n");
+	printf("나이: %d\n", 20);
+	printf("주소: 인천중구운서동\n");
+	printf("010-1234-1234\n");
+	printf("%s\n", "010-1234-1234");
+
+
+
+	int num;
+	num = 20;
+	printf("%d", num);
+
+
 }
