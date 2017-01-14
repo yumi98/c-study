2aaabcbb6f4a5a404c87eded5d8f703ad1ed1903
youme <clis9498@naver.com>
2017-01-14 12:48:30 +0900

    2017.01.13, function, 두수를 받아 두 수중 큰값을 리턴하는 함수

---
+++ prac_function_max_num.c
@@ -0,0 +1,19 @@
+﻿//2017.01.13, function
+//두수를 받아 두 수중 큰값을 리턴하는 함수
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+
+int max(int x, int y) {
+	if (x>y)
+		return x;
+	else
+		return y;
+}
+
+main() {
+	int a, b;
+	printf("두수를 입력하세요: ");
+	scanf("%d %d", &a, &b);
+
+	printf("큰값은 %d입니다\n", max(a, b));
+}
