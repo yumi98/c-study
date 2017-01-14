14c02346c83823812fda7e1dda9d5b2b1bd0d536
youme <clis9498@naver.com>
2017-01-14 12:50:11 +0900

    2017.01.13, function, 유형: 전달인자는 있고 변환값이 없다

---
+++ prac_function_void.c
@@ -0,0 +1,16 @@
+﻿//2017.01.13, function
+//유형: 전달인자는 있고 변환값이 없다
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+
+
+void a(int rev)
+{
+	printf("%d\n", rev);
+}
+
+main()
+{
+	printf("%d\n", 300);
+	a(300);
+}
