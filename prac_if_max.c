88c0450ead0e5c1ffda09a71250627d770eaf552
youme <clis9498@naver.com>
2017-01-14 12:17:52 +0900

    2017.01.11, if 세개의 수를 입력받아 큰 값을 출력

---
+++ prac_if_max.c
@@ -0,0 +1,26 @@
+﻿//2017.01.11, if 
+//세개의 수를 입력받아 큰 값을 출력하시오
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+
+main()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a > b)
+	{
+		if (a > c) {
+			printf("%d\na가 크다\n", a);
+		}
+	}
+	else if (b > c)
+	{
+		if (b > a) {
+			printf("%d\nb가 크다\n", b);
+		}
+	}
+	else {
+		printf("%d\nc가 크다\n", c);
+	}
+
+}
