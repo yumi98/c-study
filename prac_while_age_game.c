9ac1a5deedff7eb54e56efe5773653cd18451b28
youme <clis9498@naver.com>
2017-01-14 12:34:03 +0900

    2017.01.12, while 사용해서 나이 맞추기 게임

---
+++ prac_while_age_game.c
@@ -0,0 +1,28 @@
+﻿//2017.01.12, while
+//while 사용해서 나이 맞추기 게임
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+
+main()
+{
+	int count = 0;
+	int result;
+	int a = 0;
+	srand(time(NULL));
+	result = rand() % 100 + 1;
+	while (a != result) {
+		printf("나이를 입력하세요: ");
+		scanf("%d", &a);
+		count++;
+		if (a > result)
+			printf("그보단 적지요\n");
+
+		else if (a < result)
+			printf("그보단 많지요\n");
+
+		else
+			printf("맞습니다\n");
+		//break;
+	}
+	printf("비교횟수는 %d\n", count);//while
+} //main
