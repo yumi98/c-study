d5f0712255f5519d953ade16743d9889cef80398
youme <clis9498@naver.com>
2017-01-14 12:25:07 +0900

    2017.01.11, while 사용해서 100까지 3의 배수 출력

---
+++ prac_while_multiple_of_3.c
@@ -0,0 +1,14 @@
+﻿//2017.01.11, while
+//while 사용해서 100까지 3의 배수 출력
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+
+int main()
+{
+	int dx = 1;
+	while (dx < 100)
+	{
+		printf("%d\n", dx);
+		dx = dx + 2;
+	}
+}
