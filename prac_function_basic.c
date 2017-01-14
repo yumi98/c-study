4c75f52602345b9393bc777d7c565ffbe4688e65
youme <clis9498@naver.com>
2017-01-14 12:54:31 +0900

    2017.01.13, function basic

---
+++ prac_function_basic.c
@@ -0,0 +1,18 @@
+﻿//2017.01.13, function basic
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+display() //함수의 정의 //display 내가지은이름(함수명)
+{
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+	printf("홍길동\n");
+}
+main()
+{
+	display();
+}
