fc47c7e236e60dcf4bfba58e4e405da4b05602b4
youme <clis9498@naver.com>
2017-01-14 12:21:52 +0900

    2017.01.11, for이용해서 구구단만들기

---
+++ prac_for_multiplication.c
@@ -0,0 +1,16 @@
+﻿//2017.01.11, for
+//for이용해서 구구단만들기
+#include <stdio.h>
+#pragma warning(disable: 4996) 
+int main()
+{
+	int a;
+	int b;
+	for (a = 2; a <= 9; a = a + 1)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			printf("%d * %d= %d\n", a, b, a*b);
+		}
+	}
+}
