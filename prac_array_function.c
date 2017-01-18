5250cbe1b9b8063a8e795bb54c86b7030029480f
youme <clis9498@naver.com>
2017-01-19 00:01:23 +0900

    2017.01.19 배열과 함수를 사용하여 5개 중 큰값을 출력,출력 더하기

---
+++ prac_array_function.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#pragma warning(disable: 4996)
+void get_num(int *p)
+{
+	int dx;
+	for (dx = 0; dx < 5; dx++)
+		scanf("%d", p + dx);
+}
+void put_num(int *p)
+{
+	int dx;
+	for (dx = 0; dx < 5; dx++)
+		printf("%d\n", *(p + dx));
+
+}
+void all_sum(int *p)
+{
+	int dx, sum = 0;
+	for (dx = 0; dx < 5; dx++) {
+		sum += p[dx];
+	}
+	printf("%d\n", sum);
+}
+void max_num(int *p)
+{
+	int dx, max = 0;
+	max = *p;
+	for (dx = 0; dx < 5; dx++)
+		if (max < p[dx]) {
+			max = p[dx];
+		}
+	printf("%d\n", max);
+}
+main()
+{
+	int a[5];
+	get_num(a);  //5개 입력
+	put_num(a);  //5개 출력
+	all_sum(a);  //5개 총합
+	max_num(a);  //5개중 큰값
+}
